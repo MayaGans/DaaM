cdc454c522b410736ee9b53cb332a0c61ac8b49e
Maya Gans <jaffe.maya@gmail.com>
2020-08-27 16:20:17 -0700

    adding song durations

--- src/Components/Songs.svelte
+++ src/Components/Songs.svelte
@@ -1,57 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
-	import * as d3 from 'd3';
+  import * as d3 from 'd3';
+  import {allsongs} from '../Data/songs.js'
 
   let el;
   let sng = "a";
   let scoops = "AC/DC Bag";
 
-  export const initialData = [
-    {x: 5.56, y: 1, group: "AC/DC Bag", year: "1989-05-01"},
-    {x: 6.51, y: 1, group: "AC/DC Bag", year: "1994-07-08"},
-    {x: 9.01, y: 1, group: "AC/DC Bag", year: "1995-06-19"},
-    {x: 6.56, y: 1, group: "AC/DC Bag", year: "2010-08-07"},
-    {x: 7.16, y: 1, group: "AC/DC Bag", year: "2013-07-26"},
-    {x: 5.44, y: 1, group: "AC/DC Bag", year: "2015-08-12"},
-
-    {x: null, y: 1, group: "David Bowie", year: "1989-05-01"},
-    {x: 23.43, y: 1, group: "David Bowie", year: "1995-06-19"},
-    {x: 18.18, y: 1, group: "David Bowie", year: "1997-07-21"},
-    {x: 11.26, y: 1, group: "David Bowie", year: "2012-08-19"},
-    {x: 13.09, y: 1, group: "David Bowie", year: "2013-07-26"},
-    {x: 13.36, y: 1, group: "David Bowie", year: "2017-01-15"},
-
-    {x: 6.54, y: 1, group: "Free", year: "2012-08-19"},
-    {x: 6.34, y: 1, group: "Free", year: "2014-07-27"},
-    {x: 8.57, y: 1, group: "Free", year: "2015-08-12"},
-    {x: 11.12, y: 1, group: "Free", year: "2017-07-28"},
-    {x: 7.13, y: 1, group: "Free", year: "2018-08-03"},
-
-    {x: null, y: 1, group: "Possum", year: "1989-05-01"},
-    {x: 14.16, y: 1, group: "Possum", year: "1995-06-19"},
-    {x: 8.30, y: 1, group: "Possum", year: "2013-07-26"},
-    {x: 8.05, y: 1, group: "Possum", year: "2015-08-22"},
-    {x: 9.43, y: 1, group: "Possum", year: "2017-01-15"},
-    {x: 8.47, y: 1, group: "Possum", year: "2018-10-26"},
-
-    {x: 23.46, y: 1, group: "Tweezer", year: "2013-10-20"},
-    {x: 14.37, y: 1, group: "Tweezer", year: "2014-07- d27"},
-    {x: 17.36, y: 1, group: "Tweezer", year: "2015-08-22"},
-    {x: 12.08, y: 1, group: "Tweezer", year: "2017-01-15"},
-    {x: 16.26, y: 1, group: "Tweezer", year: "2018-08-03"},
-    {x: 20.13, y: 1, group: "Tweezer", year: "2018-10-26"},
-    {x: 26.88, y: 1, group: "Tweezer", year: "2018-12-29"},
-
-    {x: 15.45, y: 1, group: "You Enjoy Myself", year: "1989-05-01"},
-    {x: 19.46, y: 1, group: "You Enjoy Myself", year: "1994-07-08"},
-    {x: 24.39, y: 1, group: "You Enjoy Myself", year: "1995-06-19"},
-    {x: 19.26, y: 1, group: "You Enjoy Myself", year: "2012-08-19"},
-    {x: 19.09, y: 1, group: "You Enjoy Myself", year: "2012-08-31"},
-    {x: 21.32, y: 1, group: "You Enjoy Myself", year: "2014-07-27"},
-    {x: 17.55, y: 1, group: "You Enjoy Myself", year: "2019-07-14"}
-]
-
-  let data = initialData;
+  let data = allsongs;
 
 onMount(() => {
   makeChart()
@@ -60,62 +16,86 @@ onMount(() => {
   
 const makeChart = () => {
 
- var margin = {top: 20, right: 60, bottom: 30, left: 60},
-    width = 700 - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;
-
   var tooltip = d3.select("body")
     .append("div")
     .style("position", "absolute")
     .style("z-index", "10")
     .style("visibility", "hidden")
-    .style("color", "#DBE4DF")
+    .style("color", "black")
+    .style("background-color", "#DBE4DF")
+    .style("font-size", "10px")
+    .style("font-family", "Raleway")
 
- let x = d3.scaleLinear()
-    .domain(d3.extent(data, d => d.x)).nice()
-    .range([margin.left, width - margin.right])
+    let yAccessor = d => d.duration
+    let dateParser = d3.timeParse("%Y-%m-%d")
+    let xAccessor = d => dateParser(d.date)
 
-  let y = d3.scaleLinear()
-    .domain(d3.extent(data, d => d.y)).nice()
-    .range([height - margin.bottom, margin.top])
+    let dimensions = {
+    width: 900, 
+    height: 300,
+    margin: {
+        top: 15,
+        right: 15,
+        bottom: 40,
+        left: 70,
+      }
+    }
 
-  let xAxis = g => g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x).ticks(width / 50))
-    .call(g => g.select(".domain").remove())
-    .call(g => g.append("text")
-        .attr("x", width)
-        .attr("y", margin.bottom)
-        .attr("fill", "#DBE4DF")
-        .attr("text-anchor", "end")
-        .text(data.x))
-    const svg = d3.select(el)
+    dimensions.boundedWidth = dimensions.width
+    - dimensions.margin.left
+    - dimensions.margin.right
+
+    dimensions.boundedHeight = dimensions.height
+    - dimensions.margin.top
+    - dimensions.margin.bottom
+
+  const wrapper = d3.select(el)
       .append("svg")
-      .attr("viewBox", [0, 0, width, height]);
+      .attr("width", dimensions.width)
+      .attr("height", dimensions.height)
 
-  svg.append("g")
-      .call(xAxis)
-      .attr("font-family", "Raleway")
+  let bounds = wrapper.append("g")
+    .style("transform", `translate(${
+        dimensions.margin.left
+    }px, ${ dimensions.margin.top
+    }px)`)
 
-  svg.append("g")
+  let yScale = d3.scaleLinear()
+    .domain(d3.extent(data, yAccessor))
+    .range([dimensions.boundedHeight, 0])
+
+  let xScale = d3.scaleTime() 
+   .domain(d3.extent(data, xAccessor)) 
+   .range([0, dimensions.boundedWidth])
+
+  let lineGenerator = d3.line()
+.x(d => xScale(xAccessor(d)))
+.y(d => yScale(yAccessor(d)))
+
+  let line = bounds.append("path")
+.attr("d", lineGenerator(data))
+.attr("fill", "none") 
+.attr("stroke", "#DBE4DF")
+.attr("stroke-width", 1)
+
+bounds.append("g")
       .attr("stroke-width", 1.5)
     .selectAll("circle")
-    .data(data)
+    .data(data.filter(function (d) { return d.daam === true }))
     .join("circle")
-      .attr("cx", d => x(d.x))
-      .attr("cy", d => y(d.y))
-      .attr("r", 3)
+      .attr("cx", d => xScale(xAccessor(d)))
+      .attr("cy", d => yScale(yAccessor(d)))
+      .attr("r", 7)
       .attr("fill", "#DBE4DF")
       .attr("color", "#DBE4DF")
-      .on('mouseover', function (d, i) {
+.on('mouseover', function (d, i) {
       d3.select(this).transition()
         .duration('50')
-        .attr("r", 5)
+        .attr("r", 20)
         .attr('opacity', '.85')
-        .attr("fill", "white")
-        .attr("color", "white")
-
-      tooltip.text(d.year); 
+      .attr("fill", "#DBE4DF")
+      .attr("color", "#DBE4DF")
+      tooltip.text(d.date).attr("font-family", "Raleway")
       return tooltip.style("visibility", "visible")
       
     })
@@ -124,32 +104,31 @@ const makeChart = () => {
     .on('mouseout', function (d, i) {
       d3.select(this).transition()
         .duration('50')
-        .attr("r", 3)
+        .attr("r", 7)
         .attr('opacity', '1')
         .attr("fill", "#DBE4DF")
         .attr("color", "#DBE4DF")
-
       return tooltip.style("visibility", "hidden")
     })
     .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-20)+"px").style("left",(d3.event.pageX+20)+"px");})
 
-  svg.append("g")
-      .attr("font-size", 5)
-    .selectAll("text")
-    .data(data)
-    .join("text")
-      .attr("dy", "0.35em")
-      .attr("x", d => x(d.x) + 7)
-      .attr("y", d => y(d.y))
-      .text(d => d.name)
-      .attr("fill", "#DBE4DF")
-      .attr("color", "#DBE4DF")
-}
+    let yAxisGenerator = d3.axisLeft().scale(yScale)
+    let xAxisGenerator = d3.axisBottom().scale(xScale)
+
+    let xAxis = bounds.append("g") 
+    .call(xAxisGenerator)
+.style("transform", `translateY(${ dimensions.boundedHeight
+}px)`)
+.attr("font-family", "Raleway")
+
+let yAxis = bounds.append("g") .call(yAxisGenerator).attr("font-family", "Raleway")
+
+  }
 
 const updateChart = () => {
 
-  data = initialData.filter(function (el) {
-    return el.group === scoops
+  data = allsongs.filter(function (el) {
+    return el.song === scoops
   });
 
 	// get the chart div
@@ -186,7 +165,6 @@ const updateChart = () => {
   .float-container {
     height: 500px;
     background-color: black;
-    padding-bottom: 50px;
     text-align: left;
 }
 
